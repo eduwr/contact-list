6ba8bb80c0404bb3326aeb0143f54922da1789dd
eduwr <eduwric@gmail.com>
2022-08-18 15:47:10 -0300

    backend setup

---
+++ backend/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  rootDir: "./src",
+  // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: [ "js", "ts", "json" ],
+  moduleDirectories: [ "node_modules", "src" ],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+};
+export default config;
