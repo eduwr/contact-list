82d2d1cdac8a254e79cd74d7870934e022f3a9de
eduwr <eduwric@gmail.com>
2022-08-19 00:52:50 -0300

    call controllers on routes

--- backend/src/modules/people/people.routes.ts
+++ backend/src/modules/people/people.routes.ts
@@ -5,10 +5,10 @@ import { ControllerInterface } from "../../shared/interfaces/controller.interfac
 export class PeopleRoutes implements  RouterInterface {
   constructor(private router: Router, private peopleController: ControllerInterface) {
     this.router.get("/people", (req, res) => peopleController.index(req, res));
-    this.router.get("/people/:id", (req, res) => console.log("/people/:id"));
+    this.router.get("/people/:id", (req, res) => peopleController.show(req, res));
     this.router.post("/people", (req, res) => peopleController.create(req, res));
-    this.router.patch("/people/:id", (req, res) => console.log("PATCH /people/:id"));
-    this.router.delete("/people/:id", (req, res) => console.log("DELETE /people/:id"));
+    this.router.patch("/people/:id", (req, res) => peopleController.update(req, res));
+    this.router.delete("/people/:id", (req, res) => peopleController.delete(req, res));
   }
 
   get routes() {
