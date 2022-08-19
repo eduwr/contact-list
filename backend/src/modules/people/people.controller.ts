d384617760788a650baca7152d6cdc90f42c88f8
eduwr <eduwric@gmail.com>
2022-08-18 23:26:16 -0300

    create people controller

---
+++ backend/src/modules/people/people.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { ControllerInterface } from "../../shared/interfaces/controller.interface";
+import { IPeopleService } from "./interfaces/people.service.interface";
+import { CreatePersonDTO } from "./interfaces/createPerson.dto";
+
+export class PeopleController implements ControllerInterface {
+
+  constructor(private peopleService: IPeopleService) {
+  }
+
+  async index(req: Request, res: Response): Promise<void> {
+    const people = await this.peopleService.findAll();
+    res.status(200);
+    res.send(people);
+  }
+
+  async create(req: Request, res: Response): Promise<void> {
+    const { name, contacts } = req.body as CreatePersonDTO;
+    try {
+      const person = await this.peopleService.createPerson({ name, contacts });
+      res.status(204);
+      res.send(person);
+    } catch (e) {
+      res.status(400);
+      res.send(e)
+    }
+  }
+
+  delete(req: Request, res: Response): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  show(req: Request, res: Response): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  update(req: Request, res: Response): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+}
--- backend/src/modules/people/people.provider.ts
+++ backend/src/modules/people/people.provider.ts
@@ -3,9 +3,11 @@ import { Person } from "./person.entity";
 import { PeopleService } from "./people.service";
 import { PeopleRoutes } from "./people.routes";
 import express from "express";
+import { PeopleController } from "./people.controller";
 
 export const peopleProvider = (c: Container) => {
   c.service('PeopleRoutes', c => new PeopleRoutes(express.Router()))
   c.service('PeopleRepository', c => c.Database.getRepository(Person));
   c.service('PeopleService', c => new PeopleService(c.PeopleRepository));
+  c.service('PeopleController', c => new PeopleController(c.PeopleService));
 }
