fb3f1d1d089da3412c9445ed9783d951f4d3b861
eduwr <eduwric@gmail.com>
2022-08-20 00:23:18 -0300

    setup tailwindcss

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
+
+
+  constructor(public route: Router) {
+  }
+
+
   title = 'frontend';
 }
